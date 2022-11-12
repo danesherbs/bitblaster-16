eb92d260b5c5d2659f58590d2796fed2a116a204
Dane <danesherbs@gmail.com>
2022-11-12 20:41:16 +0000

    Update gates.py

--- gates.py
+++ gates.py
@@ -1,3 +1,6 @@
+from typing import Tuple
+
+
 # elementary logic gates
 def AND(x: bool, y: bool) -> bool:
     return x and y
@@ -9,14 +12,30 @@ def NOT(x: bool) -> bool:
     return not x
 
 def NAND(x: bool, y: bool) -> bool:
-    return NOT(AND(x, y))
+    return not(x and y)
 
 def XOR(x: bool, y: bool) -> bool:
-    return OR(
-        AND(NOT(x), y),
-        AND(x, NOT(y)),
-    )
+    return ((not x) and y) or (x and (not y))
+
+def MUX(x: bool, y: bool, sel: bool) -> bool:
+    return (x and (not y) and (not sel)) or \
+           (x and y and (not sel)) or \
+           ((not x) and y and sel) or \
+           (x and y and sel)
+
+def DMUX(x: bool, sel: bool) -> Tuple[bool, bool]:
+    return x and (not sel), x and sel
+
 
 # 16-bit variants
+def NOT16(xs: Tuple[bool]) -> Tuple[bool]:
+    assert len(xs) == 16
+    return tuple(not x for x in xs)
+
+def AND16(xs: Tuple[bool], ys: Tuple[bool]) -> Tuple[bool]:
+    assert len(xs) == 16 and len(ys) == 16
+    return tuple(x and y for x, y in zip(xs, ys))
+
 
 # multi-way variants
+# TODO: Add stubs
