491840bc15000924cb0599c43ba6ba3ca3349b66
Dane <danesherbs@gmail.com>
2022-11-13 18:06:56 +0000

    Update gates.py

--- gates.py
+++ gates.py
@@ -6,32 +6,64 @@ def AND(x: bool, y: bool) -> bool:
     """And gate."""
     return x and y
 
+
 def OR(x: bool, y: bool) -> bool:
     """Or gate."""
     return x or y
+
 
 def NOT(x: bool) -> bool:
     """Not gate."""
     return not x
 
+
 def NAND(x: bool, y: bool) -> bool:
     """Nand gate."""
-    return not(x and y)
+    return not (x and y)
+
 
 def XOR(x: bool, y: bool) -> bool:
     """Xor gate."""
     return ((not x) and y) or (x and (not y))
 
+
 def MUX(x: bool, y: bool, sel: bool) -> bool:
     """Selects between two inputs."""
-    return (x and (not y) and (not sel)) or \
-           (x and y and (not sel)) or \
-           ((not x) and y and sel) or \
-           (x and y and sel)
+    # pre-conditions
+    assert isinstance(x, bool), "`x` must be of type `bool`"
+    assert isinstance(y, bool), "`y` must be of type `bool`"
+
+    # implementation
+    out = (
+        (x and (not y) and (not sel))
+        or (x and y and (not sel))
+        or ((not x) and y and sel)
+        or (x and y and sel)
+    )
+
+    # post-conditions
+    assert isinstance(out, bool), "Output must be of type `bool`"
+
+    return out
+
 
 def DMUX(x: bool, sel: bool) -> Tuple[bool, bool]:
     """De-multiplexes a single input into two outputs."""
-    return x and (not sel), x and sel
+    # pre-conditions
+    assert isinstance(x, bool), "`x` must be of type `bool`"
+    assert isinstance(sel, bool), "`sel` must be of type `bool`"
+
+    # implementation
+    out = (x and (not sel), x and sel)
+
+    # post-conditions
+    assert (
+        isinstance(out, tuple)
+        and len(out) == 2
+        and all(isinstance(o, bool) for o in out)
+    ), "Output must be 2-bit tuple of `bool`s"
+
+    return out
 
 
 # 16-bit variants
@@ -40,40 +72,142 @@ def NOT16(xs: Tuple[bool]) -> Tuple[bool]:
     assert len(xs) == 16
     return tuple(not x for x in xs)
 
+
 def AND16(xs: Tuple[bool], ys: Tuple[bool]) -> Tuple[bool]:
     """16-bit And."""
     assert len(xs) == 16 and len(ys) == 16
     return tuple(x and y for x, y in zip(xs, ys))
+
 
 def OR16(xs: Tuple[bool], ys: Tuple[bool]) -> Tuple[bool]:
     """16-bit Or."""
     assert len(xs) == 16 and len(ys) == 16
     return tuple(x or y for x, y in zip(xs, ys))
 
+
 def MUX16(xs: Tuple[bool], ys: Tuple[bool], sel: bool) -> Tuple[bool]:
     """Selects between two 16-bit inputs."""
-    assert len(xs) == 16 and len(ys) == 16
-    return tuple(MUX(x, y, sel) for x, y in zip(xs, ys))
+    # pre-conditions
+    assert (
+        isinstance(xs, tuple) and len(xs) == 16 and all(isinstance(x, bool) for x in xs)
+    ), "`x` must be 16-bit tuple of `bool`s"
+    assert (
+        isinstance(ys, tuple) and len(ys) == 16 and all(isinstance(y, bool) for y in ys)
+    ), "`y` must be 16-bit tuple of `bool`s"
+
+    # implementation
+    out = tuple(MUX(x, y, sel) for x, y in zip(xs, ys))
+
+    # post-conditions
+    assert (
+        isinstance(out, tuple)
+        and len(out) == 16
+        and all(isinstance(o, bool) for o in out)
+    ), "Output must be 16-bit tuple of `bool`s"
+
+    return out
 
 
 # multi-way variants
 def OR8WAY(xs: Tuple[bool]) -> bool:
     """8-way Or."""
-    assert len(xs) == 8
-    return (xs[0] or (xs[1] or (xs[2] or (xs[3] or (xs[4] or (xs[5] or (xs[6] or xs[7])))))))
+    # pre-conditions
+    assert (
+        isinstance(xs, tuple) and len(xs) == 8 and all(isinstance(x, bool) for x in xs)
+    ), "`xs` must be an 8-bit tuple of `bool`s"
 
-def MUX4WAY16(xs: Tuple[bool], ys: Tuple[bool], zs: Tuple[bool], ws: Tuple[bool], sel: Tuple[bool]) -> Tuple[bool]:
+    # implementation
+    out = xs[0] or (
+        xs[1] or (xs[2] or (xs[3] or (xs[4] or (xs[5] or (xs[6] or xs[7])))))
+    )
+
+    # post-conditions
+    assert isinstance(out, bool), "Output must be of type `bool`"
+
+    return out
+
+
+def MUX4WAY16(
+    xs: Tuple[bool], ys: Tuple[bool], zs: Tuple[bool], ws: Tuple[bool], sel: Tuple[bool]
+) -> Tuple[bool]:
     """Selects between four 16-bit inputs."""
-    raise NotImplementedError()
+    # pre-conditions
+    assert (
+        isinstance(xs, tuple) and len(xs) == 16 and all(isinstance(x, bool) for x in xs)
+    ), "`xs` must be a 2-bit tuple of `bool`s"
+    assert (
+        isinstance(ys, tuple) and len(ys) == 16 and all(isinstance(y, bool) for y in ys)
+    ), "`ys` must be a 2-bit tuple of `bool`s"
+    assert (
+        isinstance(zs, tuple) and len(zs) == 16 and all(isinstance(z, bool) for z in zs)
+    ), "`zs` must be a 2-bit tuple of `bool`s"
+    assert (
+        isinstance(ws, tuple) and len(ws) == 16 and all(isinstance(w, bool) for w in ws)
+    ), "`ws` must be a 2-bit tuple of `bool`s"
+    assert (
+        isinstance(sel, tuple)
+        and len(sel) == 2
+        and all(isinstance(s, bool) for s in sel)
+    ), "`sel` must be a 2-bit tuple of `bool`s"
 
-def MUX8WAY16(xs: Tuple[bool], ys: Tuple[bool], zs: Tuple[bool], ws: Tuple[bool], us: Tuple[bool], vs: Tuple[bool], ms: Tuple[bool], ns: Tuple[bool], sel: Tuple[bool]) -> Tuple[bool]:
+    # implementation
+    out = MUX16(
+        MUX16(
+            MUX16(
+                xs,
+                ys,
+                (not sel[0]) and sel[1],
+            ),
+            zs,
+            sel[0] and (not sel[1]),
+        ),
+        ws,
+        sel[0] and sel[1],
+    )
+
+    # post-conditions
+    assert (
+        isinstance(out, tuple)
+        and len(out) == 16
+        and all(isinstance(o, bool) for o in out)
+    ), "Output must be 16-bit tuple of `bool`s"
+
+    return out
+
+
+def MUX8WAY16(
+    xs: Tuple[bool],
+    ys: Tuple[bool],
+    zs: Tuple[bool],
+    ws: Tuple[bool],
+    us: Tuple[bool],
+    vs: Tuple[bool],
+    ms: Tuple[bool],
+    ns: Tuple[bool],
+    sel: Tuple[bool],
+) -> Tuple[bool]:
     """Selects between eight 16-bit inputs."""
     raise NotImplementedError()
 
-def DMUX4WAY(xs: Tuple[bool], sel: Tuple[bool]) -> Tuple[Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool]]:
+
+def DMUX4WAY(
+    xs: Tuple[bool], sel: Tuple[bool]
+) -> Tuple[Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool]]:
     """De-multiplexes a single input into four outputs."""
     raise NotImplementedError()
 
-def DMUX8WAY(xs: Tuple[bool], sel: Tuple[bool]) -> Tuple[Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool], Tuple[bool]]:
+
+def DMUX8WAY(
+    xs: Tuple[bool], sel: Tuple[bool]
+) -> Tuple[
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+    Tuple[bool],
+]:
     """De-multiplexes a single input into eight outputs."""
     raise NotImplementedError()
