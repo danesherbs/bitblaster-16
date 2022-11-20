99992a5ae5ddb0aa797bccb38ca3b1d0b595aaaf
Dane <danesherbs@gmail.com>
2022-11-20 09:16:09 +0000

    Update test_arithmetic.py

--- test_arithmetic.py
+++ test_arithmetic.py
@@ -2,30 +2,39 @@ import arithmetic
 
 
 def test_halfadder():
-    arithmetic.HALFADDER(False, False) == (False, False)
-    arithmetic.HALFADDER(False, True) == (True, False)
-    arithmetic.HALFADDER(True, False) == (True, False)
-    arithmetic.HALFADDER(True, True) == (True, True)
+    assert arithmetic.HALFADDER(False, False) == (False, False)
+    assert arithmetic.HALFADDER(False, True) == (True, False)
+    assert arithmetic.HALFADDER(True, False) == (True, False)
+    assert arithmetic.HALFADDER(True, True) == (True, True)
 
 
 def test_fulladder():
-    arithmetic.FULLADDER(False, False, False) == (False, False)
-    arithmetic.FULLADDER(False, False, True) == (True, False)
-    arithmetic.FULLADDER(False, True, False) == (True, False)
-    arithmetic.FULLADDER(False, True, True) == (False, True)
-    arithmetic.FULLADDER(True, False, False) == (True, False)
-    arithmetic.FULLADDER(True, False, True) == (False, True)
-    arithmetic.FULLADDER(True, True, False) == (False, True)
-    arithmetic.FULLADDER(True, True, True) == (True, True)
+    assert arithmetic.FULLADDER(False, False, False) == (False, False)
+    assert arithmetic.FULLADDER(False, False, True) == (True, False)
+    assert arithmetic.FULLADDER(False, True, False) == (True, False)
+    assert arithmetic.FULLADDER(False, True, True) == (False, True)
+    assert arithmetic.FULLADDER(True, False, False) == (True, False)
+    assert arithmetic.FULLADDER(True, False, True) == (False, True)
+    assert arithmetic.FULLADDER(True, True, False) == (False, True)
+    assert arithmetic.FULLADDER(True, True, True) == (True, True)
 
 
 def test_add16():
-    arithmetic.ADD16((False,) * 16, (False,) * 16) == (False,) * 16
-    arithmetic.ADD16((False,) * 16, (True,) * 16) == (True,) * 16
-    arithmetic.ADD16((True,) * 16, (False,) * 16) == (True,) * 16
-    arithmetic.ADD16((True,) * 16, (False,) * 15 + (True,)) == (False,) * 16
+    assert arithmetic.ADD16((False,) * 16, (False,) * 16) == (False,) * 16
+    assert arithmetic.ADD16((False,) * 16, (True,) * 16) == (True,) * 16
+    assert arithmetic.ADD16((True,) * 16, (False,) * 16) == (True,) * 16
+    assert arithmetic.ADD16((True,) * 16, (False,) * 15 + (True,)) == (False,) * 16
 
 
 def test_inc16():
-    arithmetic.INC16((False,) * 16) == (False,) * 15 + (True,)
-    arithmetic.INC16((True,) * 16) == (False,) * 16
+    assert arithmetic.INC16((False,) * 16) == (False,) * 15 + (True,)
+    assert arithmetic.INC16((True,) * 16) == (False,) * 16
+
+
+def test_neg16():
+    assert arithmetic.NEG16((False,) * 16) == (False,) * 16
+    assert arithmetic.NEG16((True,) * 16) == (False,) * 15 + (True,)
+
+
+def test_alu():
+    pass
